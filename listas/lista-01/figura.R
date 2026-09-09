55cc1a64fe3e35ce0c349083a1d9e901dec5af36
Diego Lopes Vieira <diego.vieira@asibras.com>
2026-09-09 17:14:54 -0300

    Exercício 3 Lista 01 Parte 2 Pós Styler

--- listas/lista-01/figura.R
+++ listas/lista-01/figura.R
@@ -1,13 +1,13 @@
-dados = read.csv("airquality.csv")
+dados <- read.csv("airquality.csv")
 
-pdf("figura.pdf", width=7, height=5)
+pdf("figura.pdf", width = 7, height = 5)
 boxplot(
   Ozone ~ Month,
-  data=dados,
-  names=c("Maio", "Junho", "Julho", "Agosto", "Setembro"),
-  xlab="Mês",
-  ylab="Concentração de ozônio (ppb)",
-  col="lightblue",
-  pch=20
+  data = dados,
+  names = c("Maio", "Junho", "Julho", "Agosto", "Setembro"),
+  xlab = "Mês",
+  ylab = "Concentração de ozônio (ppb)",
+  col = "lightblue",
+  pch = 20
 )
 dev.off()
